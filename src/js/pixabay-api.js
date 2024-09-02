ebd48ccec0856a4f984f2ba31fc2e329590726ef
Lolita <lolita454462@gmail.com>
2024-09-02 14:38:28 +0300

    js modal cod

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,55 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+// Описаний у документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const lightbox = new SimpleLightbox('.gallery-item a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  overlayOpacity: 0.8,
+});
+import { createGallery, addElToHTML } from './render-functions';
+
+export const addLoader = loaderSpan => {
+  loaderSpan.classList.add('loader');
+};
+
+export const removeLoader = loaderSpan => {
+  loaderSpan.classList.remove('loader');
+};
+
+export const requestFetch = (galleryList, loaderSpan, inputValueRequest) => {
+  fetch(
+    `https://pixabay.com/api/?key=45720835-cd950b20c56733b9244c71a24&image_type=photo&orientation=horizontal&q=${inputValueRequest}&safesearch=true&per_page=9`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.total === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      }
+      const gallery = data.hits
+        .map(imgInform => createGallery(imgInform))
+        .join('');
+
+      addElToHTML(gallery, galleryList);
+
+      lightbox.refresh();
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    .finally(() => {
+      removeLoader(loaderSpan);
+    });
+};
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { addLoader, requestFetch } from './js/pixabay-api';
+import { cleanElHTML } from './js/render-functions';
+
+const form = document.querySelector('.form');
+const galleryList = document.querySelector('.gallery');
+const loaderSpan = document.querySelector('.form-section span');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const inputValue = form.elements.user_query.value.trim();
+
+  cleanElHTML(galleryList);
+  addLoader(loaderSpan);
+  requestFetch(galleryList, loaderSpan, inputValue);
+});
